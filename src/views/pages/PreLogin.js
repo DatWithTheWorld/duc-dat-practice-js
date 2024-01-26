c4b6ded4adc368b7b4ec0b8db862773f8ed6a4c2
= <=>
2024-01-26 19:02:32 +0700

    #5 remove loginform

---
+++ src/views/pages/PreLogin.js
@@ -0,0 +1,21 @@
+
+import {LgLogoMainIcon} from '../../resources/assets/icons';
+const PreLogin = () => {
+return `
+
+ <div class = "pk-form-prelogin">
+  <div class = "form-prelogin">
+    <div class = "form-prelogin-title">
+    <figure><img src = "${LgLogoMainIcon}"></figure>
+        <p>Enjoy the newest movies</p>
+    </div>
+    <div class = "form-prelogin-behavior">
+    <button class = "btn-primary btn-log">Log in</button>
+    <p>No account ? <a href="#" class = "btn-signup" >Sign up</a></p>
+    </div>
+ </div>
+`;
+};
+
+
+export default PreLogin;

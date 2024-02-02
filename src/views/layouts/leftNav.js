43f68e80f06a776642d4b4675ee07a0ccc038213
= <=>
2024-02-02 23:00:49 +0700

    #16 Trending page

---
+++ src/views/layouts/leftNav.js
@@ -0,0 +1,43 @@
+import { LgLogoMainIcon,
+IcFilmIcon,
+IcHeartIcon,
+IcTrendingIcon,
+IcUsersIcon,
+IcCalendarIcon,
+IcMessageIcon,
+LgBgr,
+IcSlidersIcon,
+IcLogOutIcon, } from "../../resources/assets/icons";
+import navChild from "../components/navChild";
+const homeLogo = [
+    `<figure><img src="${LgLogoMainIcon}"></figure>`
+]; 
+const listNavFirst = [
+    `<figure><img src="${IcFilmIcon}"></figure><span>Home</span>`,
+    `<figure><img src="${IcHeartIcon}"></figure><span>Favorites</span>`,
+    `<figure><img src="${IcTrendingIcon}"></figure><span>Trending</span>`,
+    `<figure><img src="${IcCalendarIcon}"></figure><span>Coming soon</span>`,
+];
+const listNavSecond = [
+    `<figure><img src="${IcUsersIcon}"></figure><span>Comunity</span>`,
+    `<figure><img src="${IcMessageIcon}"></figure><span>Social</span>`,
+];
+const listNavThird = [
+    `<figure><img src="${IcSlidersIcon}"></figure><span>Settings</span>`,
+    `<figure><img src="${IcLogOutIcon}"></figure><span>Logout</span>`,
+];
+const leftNav = () => {
+return `
+<nav class="nav-left-container">
+<ul>${navChild(homeLogo)}</ul>
+
+<ul>${navChild(listNavFirst)}</ul>
+
+<ul>${navChild(listNavSecond)}</ul>
+
+<ul>${navChild(listNavThird)}</ul>
+</nav>
+`;
+}
+
+export default leftNav;
---
+++ src/views/movie.views.js
@@ -0,0 +1,25 @@
+import { createToast, removeToast } from "./components/handleToast";
+import debounce from '../helper/debounce';
+import PreLogin from './pages/PreLogin';
+import { clearForm, collectData } from "../helper/formUtils";
+import HomePage from "./pages/HomePage";
+import Favorite from "./pages/Favorite";
+class MovieView {
+    constructor(){
+        this.app = document.querySelector('#root');
+        this.main = document.createElement('div');
+        this.main.classList.add('main');
+        this.container = document.createElement('div');
+        this.container.classList.add('container');
+        this.toastList = document.createElement('ul');
+        this.toastList.classList.add('notifications');
+        this.main.appendChild(this.container);
+        this.app.appendChild(this.toastList);
+        this.app.appendChild(this.main);
+
+        // this.container.innerHTML += PreLogin();
+        this.container.innerHTML += Favorite();
+        }   
+}
+
+export default MovieView;

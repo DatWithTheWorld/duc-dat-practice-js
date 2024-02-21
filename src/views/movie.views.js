cff6c403815cb3b48869c9967a7de3aefa0f1401
= <=>
2024-02-21 22:57:13 +0700

    ads

--- src/views/movie.views.js
+++ src/views/movie.views.js
@@ -19,8 +19,10 @@ class MovieView {
         this.app.appendChild(this.toastList);
         this.app.appendChild(this.main);
 
-        // this.container.innerHTML += PreLogin();
-        this.container.innerHTML += Playscr();
+         this.container.innerHTML += PreLogin();
+        // this.container.innerHTML += Playscr();
+        // this.container.innerHTML += HomePage();
+        // this.container.innerHTML += Favorite();
         }   
 }
 

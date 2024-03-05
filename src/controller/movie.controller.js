1aca3e6edb05e634a280430d9323e554105c0da3
= <=>
2024-03-05 23:25:22 +0700

    #38 videoTiming

---
+++ src/controller/movie.controller.js
@@ -0,0 +1,29 @@
+import MovieView from '../views/movie.views';
+
+class MovieController {
+  constructor(movieServices, movieViews, userController) {
+    this.movieServices = movieServices;
+    this.movieViews = movieViews;
+    const currentPath = window.location.pathname;
+    if(currentPath === "/login"){
+      this.movieViews.toggleLogin();
+      this.movieViews.toggleRegist();
+      this.movieViews.loginss();
+    }else if(currentPath === "/playscr"){
+    this.movieViews.appearToolbar();
+    this.movieViews.toggleBar();
+    this.movieViews.videoDuration();
+    this.movieViews.return();
+  }else if(currentPath === "/home" || currentPath === "/favorite"|| currentPath === "/trending"){
+     this.movieServices.getAllMovies().then((movies)=>{
+      this.movieViews.displayData(movies)
+      this.movieViews.toggleAddForm();
+     })
+     this.movieViews.homepageoption();
+     this.movieViews.showSite();
+  }
+}
+
+}
+
+export default MovieController;

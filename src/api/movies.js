25567e7d5f24d718d363077b7cb51b51df0777c7
YUPP4 <131734339+DatWithTheWorld@users.noreply.github.com>
2024-03-17 15:08:55 +0700

    Update movies.js

--- src/api/movies.js
+++ src/api/movies.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const host = 'https://json-servers-d58dyas7a-datwiththeworlds-projects.vercel.app/';
+const host = 'https://json-servers-kwwbaq0w2-datwiththeworlds-projects.vercel.app/';
 
 export default axios.create({
   baseURL: host,
